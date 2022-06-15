8c87d3040b36144f19e42e08de310c45b63db6fe
Hoàng Văn Kiên <kiendaotac92@gmail.com>
2022-06-15 10:30:23 +0700

    add Filament admin panel

--- app/Enums/StatusEnum.php
+++ app/Enums/StatusEnum.php
@@ -6,4 +6,9 @@ final class StatusEnum
 {
     public const ACTIVE = 'ACTIVE';
     public const INACTIVE = 'INACTIVE';
+
+    const VALUE = [
+        self::ACTIVE => 'Kích hoạt',
+        self::INACTIVE => 'Không kích hoạt'
+    ];
 }
--- app/Models/Category.php
+++ app/Models/Category.php
@@ -4,15 +4,21 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 
 class Category extends Model
 {
     use HasFactory;
 
-    protected $fillable = ['title', 'description', 'status'];
+    protected $fillable = ['parent_id', 'title', 'description', 'order', 'status'];
 
-    public function recipes()
+    public function recipes(): BelongsToMany
     {
         return $this->belongsToMany(Recipe::class);
+    }
+
+    public function parent()
+    {
+        return $this->belongsTo(Category::class);
     }
 }
