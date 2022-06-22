d756ed0f08b5e0a641d2f89929e6e180793a2cf6
Hoàng Văn Kiên <kiendaotac92@gmail.com>
2022-06-23 02:27:28 +0700

    check liked

--- app/Http/Resources/RecipeResource.php
+++ app/Http/Resources/RecipeResource.php
@@ -3,6 +3,7 @@
 namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\Auth;
 
 class RecipeResource extends JsonResource
 {
@@ -14,6 +15,6 @@ class RecipeResource extends JsonResource
      */
     public function toArray($request)
     {
-        return parent::toArray($request);
+        return array_merge(parent::toArray($request), ['liked' => Auth::user()->likes()->whereId($this->id)->count() > 0]);
     }
 }
